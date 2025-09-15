02b92790b11d541b92fdb62733b76efa78a925ef
ethantseng <tseng8851@mydusd.org>
2025-09-15 11:47:01 -0700

    Add Starter Code

---
+++ EscapeRoom.java
@@ -0,0 +1,75 @@
+/*
+* Problem 1: Escape Room
+* 
+* V1.0
+* 10/10/2019
+* Copyright(c) 2019 PLTW to present. All rights reserved
+*/
+import java.util.Scanner;
+
+/**
+ * Create an escape room game where the player must navigate
+ * to the other side of the screen in the fewest steps, while
+ * avoiding obstacles and collecting prizes.
+ */
+public class EscapeRoom
+{
+  /* TO-DO: Process game commands from user input:
+      right, left, up, down: move player size of move, m, if player try to go off grid or bump into wall, score decreases
+      jump over 1 space: player cannot jump over walls
+      pick up prize: score increases, if there is no prize, penalty
+      help: display all possible commands
+      end: reach the far right wall, score increase, game ends, if game ends without reaching far right wall, penalty
+      replay: shows number of player steps and resets the board, player or another player can play the same board
+        
+      if player land on a trap, spring a trap to increase score: the program must first check if there is a trap, if none exists, penalty
+      Note that you must adjust the score with any method that returns a score
+      Optional: create a custom image for player - use the file player.png on disk
+    */
+
+  public static void main(String[] args) 
+  {      
+    // welcome message
+    System.out.println("Welcome to EscapeRoom!");
+    System.out.println("Get to the other side of the room, avoiding walls and invisible traps,");
+    System.out.println("pick up all the prizes.\n");
+    
+    GameGUI game = new GameGUI();
+    game.createBoard();
+
+    // size of move
+    int m = 60; 
+    // individual player moves
+    int px = 0;
+    int py = 0; 
+    
+    int score = 0;
+
+    Scanner in = new Scanner(System.in);
+    String[] validCommands = { "right", "left", "up", "down", "r", "l", "u", "d",
+    "jump", "jr", "jumpleft", "jl", "jumpup", "ju", "jumpdown", "jd",
+    "pickup", "p", "quit", "q", "replay", "help", "?"};
+  
+    // set up game
+    boolean play = true;
+    while (play)
+    {
+
+      // get user command and validate
+      System.out.print("Enter command:");
+      String input = UserInput.getValidInput(validCommands);
+
+	    /* process user commands*/
+    
+      /* uncomment when user quits */
+      // play = false;
+    }
+
+    score += game.endGame();
+
+    System.out.println("score=" + score);
+    System.out.println("steps=" + game.getSteps());
+  }
+}
+
+        
